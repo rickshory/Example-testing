706c13e2512c4854c500e65a47ec6629eaca0202
Rick Shory <rickshory@gmail.com>
2019-12-05 18:04:00 -0800

    "Arrange, Act, Assert"

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -1,5 +1,6 @@
 package com.example.android.architecture.blueprints.todoapp.statistics
 
+import com.example.android.architecture.blueprints.todoapp.data.Task
 import org.junit.Assert.*
 import org.junit.Test
 
@@ -8,9 +9,15 @@ class StatisticsUtilsTest {
     @Test
     fun getActiveAndCompletedStats_noCompleted_returnsHundredZero() {
         // Create an active task
+        val tasks = listOf<Task>(
+                Task("title","desc", isCompleted = false)
+        )
 
         // Call your function
+        val result = getActiveAndCompletedStats(tasks)
 
         // Check the result
+        assertEquals(result.completedTasksPercent, 0f)
+        assertEquals(result.activeTasksPercent, 100f)
     }
 }
