48b0c2132fddebf2084c2f636fdb952279699c4a
Rick Shory <rickshory@gmail.com>
2019-12-05 17:38:41 -0800

    testing fn stub

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -2,4 +2,13 @@ package com.example.android.architecture.blueprints.todoapp.statistics
 
 import org.junit.Assert.*
 
-class StatisticsUtilsTest
+class StatisticsUtilsTest {
+
+    fun getActiveAndCompletedStats_noCompleted_returnsHundredZero() {
+        // Create an active task
+
+        // Call your function
+
+        // Check the result
+    }
+}
