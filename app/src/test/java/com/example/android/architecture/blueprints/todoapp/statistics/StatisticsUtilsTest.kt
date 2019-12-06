77284843c96a8e033130092e30e340c1ea196316
Rick Shory <rickshory@gmail.com>
2019-12-05 17:55:47 -0800

    @Test annotation

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -1,9 +1,11 @@
 package com.example.android.architecture.blueprints.todoapp.statistics
 
 import org.junit.Assert.*
+import org.junit.Test
 
 class StatisticsUtilsTest {
 
+    @Test
     fun getActiveAndCompletedStats_noCompleted_returnsHundredZero() {
         // Create an active task
 
