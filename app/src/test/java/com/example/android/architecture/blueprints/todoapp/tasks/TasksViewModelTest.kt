18a2bd73a0a0dff8d8b06cbf1be6e249e1dbd033
Rick Shory <rickshory@gmail.com>
2019-12-08 10:02:28 -0800

    RunWith annotation

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,8 +1,11 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.*
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
 
     @Test
