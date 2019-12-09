7a816c7af710a86631e45f0a3e9872d09d9f7075
Rick Shory <rickshory@gmail.com>
2019-12-08 19:15:23 -0800

    use @Before

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -5,19 +5,29 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
+
+    // Subject under test
+    private lateinit var tasksViewModel: TasksViewModel
+
+    // Executes each task synchronously using Architecture Components
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
 
+    @Before
+    fun setupViewModel() {
+        tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+    }
+
     @Test
     fun addNewTask_setsNewTaskEvent() {
-        // Given a fresh TaskViewModel
-        val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+
         // When adding a new task
         tasksViewModel.addNewTask()
         // Then the new task event is triggered
@@ -27,8 +37,6 @@ class TasksViewModelTest {
 
     @Test
     fun setFilterAllTasks_tasksAddViewVisible() {
-        // Given a fresh TaskViewModel
-        val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
         // Set filtering to all tasks
         tasksViewModel.setFiltering(TasksFilterType.ALL_TASKS)
         // Then the the Add task button is visible
