7333ea6f7b02796969b80348252b38aea8fb0913
Rick Shory <rickshory@gmail.com>
2019-12-10 22:24:02 -0800

    member vars

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/data/source/DefaultTasksRepositoryTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/data/source/DefaultTasksRepositoryTest.kt
@@ -1,5 +1,14 @@
 package com.example.android.architecture.blueprints.todoapp.data.source
 
+import com.example.android.architecture.blueprints.todoapp.data.Task
 import org.junit.Assert.*
 
-class DefaultTasksRepositoryTest
+class DefaultTasksRepositoryTest {
+    private val task1 = Task("Title1", "Description1")
+    private val task2 = Task("Title2", "Description2")
+    private val task3 = Task("Title3", "Description3")
+    private val remoteTasks = listOf(task1, task2).sortedBy { it.id}
+    private val localTasks = listOf(task3).sortedBy { it.id }
+    private val newTasks = listOf(task3).sortedBy { it.id }
+
+}
