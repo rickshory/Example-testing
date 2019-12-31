41ea348cb8c2a353d5c011b1aade248144029b6c
Rick Shory <rickshory@gmail.com>
2019-12-30 18:37:25 -0800

    annotations

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
@@ -1,5 +1,12 @@
 package com.example.android.architecture.blueprints.todoapp.taskdetail
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
 import org.junit.Assert.*
+import org.junit.runner.RunWith
 
-class TaskDetailFragmentTest
+@MediumTest
+@RunWith(AndroidJUnit4::class)
+class TaskDetailFragmentTest {
+    
+}
