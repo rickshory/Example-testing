ef79ce3379f5499f096c15f68b3284141f976313
Rick Shory <rickshory@gmail.com>
2019-12-31 19:45:39 -0800

    ServiceLocator

---
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/ServiceLocator.kt
@@ -0,0 +1,10 @@
+package com.example.android.architecture.blueprints.todoapp
+
+import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
+import com.example.android.architecture.blueprints.todoapp.data.source.local.ToDoDatabase
+
+object ServiceLocator {
+    private var database: ToDoDatabase? = null
+    @Volatile
+    var tasksRepository: TasksRepository? = null
+}
