97d0e0e06049aab9ec911eb9fb595125f285eafa
Rick Shory <rickshory@gmail.com>
2019-12-31 19:55:40 -0800

    createDataBase fn

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/ServiceLocator.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/ServiceLocator.kt
@@ -1,5 +1,7 @@
 package com.example.android.architecture.blueprints.todoapp
 
+import android.content.Context
+import androidx.room.Room
 import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
 import com.example.android.architecture.blueprints.todoapp.data.source.local.ToDoDatabase
 
@@ -7,4 +9,13 @@ object ServiceLocator {
     private var database: ToDoDatabase? = null
     @Volatile
     var tasksRepository: TasksRepository? = null
+
+    private fun createDataBase(context: Context): ToDoDatabase {
+        val result = Room.databaseBuilder(
+                context.applicationContext,
+                ToDoDatabase::class.java, "Tasks.db"
+        ).build()
+        database = result
+        return result
+    }
 }
