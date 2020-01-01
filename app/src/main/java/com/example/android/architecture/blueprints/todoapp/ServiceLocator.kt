4cad42d1117d72fc9d64f71b1d711846d4d907c7
Rick Shory <rickshory@gmail.com>
2019-12-31 19:59:25 -0800

    createTaskLocalDataSource fn

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/ServiceLocator.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/ServiceLocator.kt
@@ -2,7 +2,9 @@ package com.example.android.architecture.blueprints.todoapp
 
 import android.content.Context
 import androidx.room.Room
+import com.example.android.architecture.blueprints.todoapp.data.source.TasksDataSource
 import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
+import com.example.android.architecture.blueprints.todoapp.data.source.local.TasksLocalDataSource
 import com.example.android.architecture.blueprints.todoapp.data.source.local.ToDoDatabase
 
 object ServiceLocator {
@@ -10,6 +12,11 @@ object ServiceLocator {
     @Volatile
     var tasksRepository: TasksRepository? = null
 
+    private fun createTaskLocalDataSource(context: Context): TasksDataSource {
+        val database = database ?: createDataBase(context)
+        return TasksLocalDataSource(database.taskDao())
+    }
+    
     private fun createDataBase(context: Context): ToDoDatabase {
         val result = Room.databaseBuilder(
                 context.applicationContext,
