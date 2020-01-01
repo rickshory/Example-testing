5c8570c9cffab9b95805585bb57ececc0afa7e77
Rick Shory <rickshory@gmail.com>
2019-12-31 20:08:59 -0800

    provideTasksRepository fn

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/ServiceLocator.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/ServiceLocator.kt
@@ -14,6 +14,12 @@ object ServiceLocator {
     @Volatile
     var tasksRepository: TasksRepository? = null
 
+    fun provideTasksRepository(context: Context): TasksRepository {
+        synchronized(this) {
+            return tasksRepository ?: createTasksRepository(context)
+        }
+    }
+
     private fun createTasksRepository(context: Context): TasksRepository {
         val newRepo = DefaultTasksRepository(TasksRemoteDataSource, createTaskLocalDataSource(context))
         tasksRepository = newRepo
