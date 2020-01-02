5e022d03307d171bbe6b56dae01b8ecbecd12b27
Rick Shory <rickshory@gmail.com>
2020-01-01 20:23:54 -0800

    implement the methods

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/source/FakeAndroidTestRepository.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/source/FakeAndroidTestRepository.kt
@@ -2,67 +2,111 @@ package com.example.android.architecture.blueprints.todoapp.source
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.map
+import com.example.android.architecture.blueprints.todoapp.data.Result
+import com.example.android.architecture.blueprints.todoapp.data.Result.Error
+import com.example.android.architecture.blueprints.todoapp.data.Result.Success
 import com.example.android.architecture.blueprints.todoapp.data.Task
 import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
+
+import kotlinx.coroutines.runBlocking
+import java.util.LinkedHashMap
 
 class FakeAndroidTestRepository : TasksRepository {
     var tasksServiceData: LinkedHashMap<String, Task> = LinkedHashMap()
     private var shouldReturnError = false
     private val observableTasks = MutableLiveData<Result<List<Task>>>()
 
+    fun setReturnError(value: Boolean) {
+        shouldReturnError = value
+    }
+
     override suspend fun refreshTasks() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        observableTasks.value = getTasks()
     }
 
     override suspend fun refreshTask(taskId: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        refreshTasks()
     }
 
-    override fun observeTasks(): LiveData<com.example.android.architecture.blueprints.todoapp.data.Result<List<Task>>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun observeTasks(): LiveData<Result<List<Task>>> {
+        runBlocking { refreshTasks() }
+        return observableTasks
     }
 
-    override fun observeTask(taskId: String): LiveData<com.example.android.architecture.blueprints.todoapp.data.Result<Task>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun observeTask(taskId: String): LiveData<Result<Task>> {
+        runBlocking { refreshTasks() }
+        return observableTasks.map { tasks ->
+            when (tasks) {
+                is Result.Loading -> Result.Loading
+                is Error -> Error(tasks.exception)
+                is Success -> {
+                    val task = tasks.data.firstOrNull() { it.id == taskId}
+                            ?: return@map Error(Exception("Not found"))
+                    Success(task)
+                }
+            }
+        }
     }
 
-    override suspend fun getTask(taskId: String, forceUpdate: Boolean): com.example.android.architecture.blueprints.todoapp.data.Result<Task> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override suspend fun getTask(taskId: String, forceUpdate: Boolean): Result<Task> {
+        if (shouldReturnError) {
+            return Error(Exception("Test exception"))
+        }
+        tasksServiceData[taskId]?.let {
+            return Success(it)
+        }
+        return Error(Exception("Could not find task"))
     }
 
-    override suspend fun getTasks(forceUpdate: Boolean): com.example.android.architecture.blueprints.todoapp.data.Result<List<Task>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override suspend fun getTasks(forceUpdate: Boolean): Result<List<Task>> {
+        if (shouldReturnError) {
+            return Error(Exception("Test exception"))
+        }
+        return Success(tasksServiceData.values.toList())
     }
 
     override suspend fun saveTask(task: Task) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        tasksServiceData[task.id] = task
     }
 
     override suspend fun completeTask(task: Task) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val completedTask = Task(task.title, task.description, true, task.id)
+        tasksServiceData[task.id] = completedTask
     }
 
     override suspend fun completeTask(taskId: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        // Not required for the remote data source.
+        throw NotImplementedError()
     }
 
     override suspend fun activateTask(task: Task) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val activeTask = Task(task.title, task.description, false, task.id)
+        tasksServiceData[task.id] = activeTask
     }
 
     override suspend fun activateTask(taskId: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        throw NotImplementedError()
     }
 
     override suspend fun clearCompletedTasks() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        tasksServiceData = tasksServiceData.filterValues {
+            !it.isCompleted
+        } as LinkedHashMap<String, Task>
     }
 
     override suspend fun deleteTask(taskId: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        tasksServiceData.remove(taskId)
     }
 
     override suspend fun deleteAllTasks() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        tasksServiceData.clear()
+    }
+
+    fun addTasks(vararg tasks: Task) {
+        for (task in tasks) {
+            tasksServiceData[task.id] = task
+        }
+        runBlocking { refreshTasks() }
     }
 }
