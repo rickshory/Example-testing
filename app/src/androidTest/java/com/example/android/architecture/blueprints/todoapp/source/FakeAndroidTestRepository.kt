1c15184a97cc76bf806cb327561914c2fbf76dfe
Rick Shory <rickshory@gmail.com>
2020-01-01 19:40:59 -0800

    stubs of overrride methods

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/source/FakeAndroidTestRepository.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/source/FakeAndroidTestRepository.kt
@@ -1,5 +1,6 @@
 package com.example.android.architecture.blueprints.todoapp.source
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.android.architecture.blueprints.todoapp.data.Task
 import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
@@ -9,4 +10,59 @@ class FakeAndroidTestRepository : TasksRepository {
     private var shouldReturnError = false
     private val observableTasks = MutableLiveData<Result<List<Task>>>()
 
+    override suspend fun refreshTasks() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun refreshTask(taskId: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun observeTasks(): LiveData<com.example.android.architecture.blueprints.todoapp.data.Result<List<Task>>> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun observeTask(taskId: String): LiveData<com.example.android.architecture.blueprints.todoapp.data.Result<Task>> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun getTask(taskId: String, forceUpdate: Boolean): com.example.android.architecture.blueprints.todoapp.data.Result<Task> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun getTasks(forceUpdate: Boolean): com.example.android.architecture.blueprints.todoapp.data.Result<List<Task>> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun saveTask(task: Task) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun completeTask(task: Task) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun completeTask(taskId: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun activateTask(task: Task) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun activateTask(taskId: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun clearCompletedTasks() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun deleteTask(taskId: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun deleteAllTasks() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
